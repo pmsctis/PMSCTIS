e67d5bf87217070cbaa0fe71771d56fdb11df202
sametsahinnet <sametsahin.php@gmail.com>
2025-02-08 11:54:47 +0300

    Initial commit

---
+++ CORE/APP/Domain/Entity.cs
@@ -0,0 +1,28 @@
+﻿namespace CORE.APP.Domain
+{
+    public abstract class Entity
+    {
+        //private int id; // variable, field
+
+        //public int GetId() // function, method, behavior
+        //{
+        //    return id;
+        //}
+
+        //public void SetId(int id)
+        //{
+        //    this.id = id;
+        //}
+
+        public int Id { get; set; } // property, Entity entity = new Entity() { Id = 17 };
+
+        protected Entity()
+        {
+        }
+
+        protected Entity(int id) // Entity entity = new Entity(19);
+        {
+            Id = id;
+        }
+    }
+}
